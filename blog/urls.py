67a29f67aa4029ea127a80597ce17df9da367fed
suhyeon <zcxv3826@naver.com>
2022-06-02 03:44:20 +0900

    골격생성

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('aboutMe', views.aboutMe, name='aboutMe'),
+    path('Portfolio', views.Portfolio, name='Portfolio'),
+    path('Contact', views.Contact, name='Contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def aboutMe(request):
+    return render(request, 'blog/aboutMe.html', {})
+
+def Portfolio(request):
+    return render(request, 'blog/Portfolio.html', {})
+
+def Contact(request):
+    return render(request, 'blog/Contact.html', {})
