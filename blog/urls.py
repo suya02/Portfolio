69cd9f44e98e36ab5ba912901a345aa947211190
suhyeon <zcxv3826@naver.com>
2022-06-06 17:12:13 +0900

    AboutMe made

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.main, name='main'),
     path('aboutMe', views.aboutMe, name='aboutMe'),
     path('Portfolio', views.Portfolio, name='Portfolio'),
     path('Contact', views.Contact, name='Contact'),
